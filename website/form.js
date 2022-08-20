1e194eadf856f80fa8e8220cbfe812a02129f449
2004ARYAN <106236703+2004ARYAN@users.noreply.github.com>
2022-08-20 20:14:06 +0530

    Add files via upload

---
+++ website/form.js
@@ -0,0 +1,17 @@
+/** function sendemail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "cocchampion567890@gmail.com",
+        Password : "02082003123456",
+        To : 'priyanshumudgal175@gmail.com',
+        From : document.getElementById('email').value,
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+} */
+
+function sendemail(){
+  console.warn("warning");
+}
---
+++ website/moontrip.js
@@ -0,0 +1,8 @@
+function showbar(){
+    const a=document.querySelector('.sidebar');
+    a.classList.add('bar')
+}
+function closemenu(){
+    const a=document.querySelector('.sidebar');
+    a.classList.remove('bar')
+}
